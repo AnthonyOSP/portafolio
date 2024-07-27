4319a3fee22a92fb7f3dba5303aa64d31b85c04a
Anthony Saavedra <anthonysaa93@gmail.com>
2024-07-27 14:31:21 -0500

    error en las comillas de about.tsx

--- app/ui/about/about.tsx
+++ app/ui/about/about.tsx
@@ -17,7 +17,7 @@ export default function about() {
                     turning complex problems into simple, beautiful and intuitive designs.
                 </p>
 
-                <p className='mb-[15px]'>
+                <p className="mb-[15px]">
                     My job is to build your website so that it is functional and user-friendly but at the same time attractive.
                     Moreover, I
                     add personal touch to your product and make sure that is eye-catching and easy to use. My aim is to bring
@@ -133,9 +133,9 @@ export default function about() {
                             <figure className="testimonials-avatar-box absolute top-0 left-0 -translate-y-[25px] translate-x-[15px] bg-gradient-to-br from-[#3f3f40] from-[3%* to-[#303030] to-97% rounded-[14px] shadow-[-4_8px_24px_0_rgba(0,0,0,0.251)] ">
                                 <Image
                                     src="/images/avatar-1.png"
-                                    alt='Henry william'
-                                    height='60'
-                                    width='60'
+                                    alt="Henry william"
+                                    height="60"
+                                    width="60"
                                     data-testimonials-avatar
                                 />
                             </figure>
@@ -161,9 +161,9 @@ export default function about() {
                             <figure className="testimonials-avatar-box absolute top-0 left-0 -translate-y-[25px] translate-x-[15px] bg-gradient-to-br from-[#3f3f40] from-[3%* to-[#303030] to-97% rounded-[14px] shadow-[-4_8px_24px_0_rgba(0,0,0,0.251)]">
                                 <Image
                                     src="/images/avatar-2.png"
-                                    alt='Henry william'
-                                    height='60'
-                                    width='60'
+                                    alt="Henry william"
+                                    height="60"
+                                    width="60"
                                     data-testimonials-avatar
                                 />
                             </figure>
@@ -189,9 +189,9 @@ export default function about() {
                             <figure className="testimonials-avatar-box absolute top-0 left-0 -translate-y-[25px] translate-x-[15px] bg-gradient-to-br from-[#3f3f40] from-[3%* to-[#303030] to-97% rounded-[14px] shadow-[-4_8px_24px_0_rgba(0,0,0,0.251)]">
                                 <Image
                                     src="/images/avatar-3.png"
-                                    alt='Emily evans'
-                                    height='60'
-                                    width='60'
+                                    alt="Emily evans"
+                                    height="60"
+                                    width="60"
                                     data-testimonials-avatar
                                 />
                             </figure>
@@ -217,9 +217,9 @@ export default function about() {
                             <figure className="testimonials-avatar-box absolute top-0 left-0 -translate-y-[25px] translate-x-[15px] bg-gradient-to-br from-[#3f3f40] from-[3%* to-[#303030] to-97% rounded-[14px] shadow-[-4_8px_24px_0_rgba(0,0,0,0.251)]">
                                 <Image
                                     src="/images/avatar-4.png"
-                                    alt='Henry william'
-                                    height='60'
-                                    width='60'
+                                    alt="Henry william"
+                                    height="60"
+                                    width="60"
                                     data-testimonials-avatar
                                 />
                             </figure>
@@ -250,12 +250,12 @@ export default function about() {
                 <section className="testimonials-modal bg-[#1e1e1f] relative p-[15px] my-[15px] mx-[12px] border-[1px] border-solid border-[#383838] rounded-[14px] shadow-[0_24px_80px_0px_rgba(0,0,0,0.25)] scale-125 opacity-[0] duration-500 ease-in-out z-[2]">
 
                     <button className="modal-close-btn absolute top-[15px] right-[15px] bg-[#2b2b2c] rounded-[8px] w-[32px] h-[32px] flex content-center items-center text-[#fafafa] text-[18px] opacity-70 hover:opacity-100 focus:opacity-100" data-modal-close-btn>
-                        <XMarkIcon  className='w-[50px]'/>
+                        <XMarkIcon  className="w-[50px]"/>
                     </button>
 
                     <div className="modal-img-wrapper">
                         <figure className="modal-avatar-box g-gradient-to-br from-[#3f3f40] to-3% to-[#303030] to-97% w-max rounded-[14px] mb-[15px] shadow-[0_16px_30px_rgba(0,0,0,0.25)] ">
-                            <img src="./images/avatar-1.png" alt="Daniel lewis" width="80" className='hidden' data-modal-img />
+                            <img src="./images/avatar-1.png" alt="Daniel lewis" width="80" className="hidden" data-modal-img />
                         </figure>
 
                         <img src="./images/icon-quote.svg" alt="quote icon" className='hidden' />
@@ -265,10 +265,10 @@ export default function about() {
 
                         <h4 className="h3 modal-title mb-[4px]" data-modal-title>Daniel lewis</h4>
 
-                        <time dateTime="2021-06-14" className='text-[14px] text-[d6d6d6b3] font-300 mb-[10px]'>14 June, 2021</time>
+                        <time dateTime="2021-06-14" className="text-[14px] text-[d6d6d6b3] font-300 mb-[10px]">14 June, 2021</time>
 
                         <div data-modal-text>
-                            <p className='text-[#d6d6d6] text-[14px] font-300 leading-[1.6]'>
+                            <p className="text-[#d6d6d6] text-[14px] font-300 leading-[1.6]">
                                 Richard was hired to create a corporate identity. We were very pleased with the work done. She has a
                                 lot of experience
                                 and is very concerned about the needs of client. Lorem ipsum dolor sit amet, ullamcous cididt
@@ -291,37 +291,37 @@ export default function about() {
 
                     <li className="clients-item min-w-[50%] snap-start">
                         <a href="#">
-                            <img src="./images/logo-1-color.png" alt="client logo" className='w-full grayscale-[1] duration-[250ms] ease-linear hover:grayscale-0'/>
+                            <img src="./images/logo-1-color.png" alt="client logo" className="w-full grayscale-[1] duration-[250ms] ease-linear hover:grayscale-0"/>
                         </a>
                     </li>
 
                     <li className="clients-item min-w-[50%] snap-start">
                         <a href="#">
-                            <img src="./images/logo-2-color.png" alt="client logo" className='w-full grayscale-[1] duration-[250ms] ease-linear hover:grayscale-0'/>
+                            <img src="./images/logo-2-color.png" alt="client logo" className="w-full grayscale-[1] duration-[250ms] ease-linear hover:grayscale-0"/>
                         </a>
                     </li>
 
                     <li className="clients-item min-w-[50%] snap-start">
                         <a href="#">
-                            <img src="./images/logo-3-color.png" alt="client logo" className='w-full grayscale-[1] duration-[250ms] ease-linear hover:grayscale-0'/>
+                            <img src="./images/logo-3-color.png" alt="client logo" className="w-full grayscale-[1] duration-[250ms] ease-linear hover:grayscale-0"/>
                         </a>
                     </li>
 
                     <li className="clients-item min-w-[50%] snap-start">
                         <a href="#">
-                            <img src="./images/logo-4-color.png" alt="client logo" className='w-full grayscale-[1] duration-[250ms] ease-linear hover:grayscale-0'/>
+                            <img src="./images/logo-4-color.png" alt="client logo" className="w-full grayscale-[1] duration-[250ms] ease-linear hover:grayscale-0"/>
                         </a>
                     </li>
 
                     <li className="clients-item min-w-[50%] snap-start">
                         <a href="#">
-                            <img src="./images/logo-5-color.png" alt="client logo" className='w-full grayscale-[1] duration-[250ms] ease-linear hover:grayscale-0'/>
+                            <img src="./images/logo-5-color.png" alt="client logo" className="w-full grayscale-[1] duration-[250ms] ease-linear hover:grayscale-0"/>
                         </a>
                     </li>
 
                     <li className="clients-item min-w-[50%] snap-start">
                         <a href="#">
-                            <img src="./images/logo-6-color.png" alt="client logo" className='w-full grayscale-[1] duration-[250ms] ease-linear hover:grayscale-0'/>
+                            <img src="./images/logo-6-color.png" alt="client logo" className="w-full grayscale-[1] duration-[250ms] ease-linear hover:grayscale-0"/>
                         </a>
                     </li>
 
